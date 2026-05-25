a43c0e695fc00b4bdf6097ed46b0cf4c0de4a313
Henri Myrto <hmtest35@yahoo.com>
2026-05-25 12:17:47 +0200

    final

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: true,
+    html: true,
+    json: true,
+    reportFilename: "report-[name]-[datetime]",
+    timestamp: "mmddyyyy_HHMMss",
+    charts: true,
+    embeddedScreenshots: true,
+  },
+
   e2e: {
-    baseUrl: 'https://www.saucedemo.com',
+    baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
